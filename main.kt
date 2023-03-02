8c72dc0424e239f6f66f1e00720ae817c2bd65a0
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-03-02 15:50:16 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,13 +1,30 @@
 package minesweeper
+import kotlin.random.Random
 
 var minedField = mutableListOf(mutableListOf<Char>())
 
 fun createField(x: Int, y: Int, mines: Int) {
     minedField = MutableList(x) {MutableList(y) {'.'}}
+    var mines = mines
+    while (mines != 0) {
+        minedField[Random.nextInt(0, x)][Random.nextInt(0, y)] = 'X'
+        mines--
+    }
 }
 
+// fun random(n: Int): List<Int> { // tried to create a randomizer function, maybe will finish in future
+//     var res = mutableListOf<Int>() // I just don't know, how to get seconds of current time
+//     var x = 8
+//     for (i in 0..n) {
+//         x = (7 * x + 8) % 11
+//         res.add(x)
+//     }
+//     return res
+// }
+    
 fun main() {
-    createField(9, 9, 10)
+    println("How many mines do you want on the field?")
+    createField(9, 9, readln().toInt())
     for (line in minedField) {
         println(line.joinToString(""))
     }

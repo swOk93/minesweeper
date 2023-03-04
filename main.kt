26c3ee61154a81be648a51827a3c077b318d53b6
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-03-04 13:44:18 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -2,14 +2,43 @@ package minesweeper
 import kotlin.random.Random
 
 var minedField = mutableListOf(mutableListOf<Char>())
+var OpenField = mutableListOf(mutableListOf<Char>())
+
+fun createOpenField() {
+    for (i in minedField.indices) { // i is a rows
+        for (j in minedField[i].indices) { // j is a columns
+            if (minedField[i][j] == 'X') {
+                OpenField[i][j] = 'X'
+                continue
+            }
+            var countOfMines = 0
+            for (x in i-1..i+1) { // x is rows from previous to next
+                for (y in j-1..j+1) {  // y is columns from previous to next
+                    if ((x >= 0 && y >= 0) && (x < minedField.size && y < minedField.size)) {
+                        if (minedField[x][y] == 'X') {
+//                            print("mine!")
+                            countOfMines = countOfMines + 1
+                        }
+                    }
+                }
+            }
+            if (countOfMines > 0) OpenField[i][j] = countOfMines.digitToChar()
+        }
+    }
+}
 
 fun createField(x: Int, y: Int, mines: Int) {
     minedField = MutableList(x) {MutableList(y) {'.'}}
+    OpenField = MutableList(x) {MutableList(y) {'.'}}
     var mines = mines
     while (mines != 0) {
-        minedField[Random.nextInt(0, x)][Random.nextInt(0, y)] = 'X'
-        mines--
+        var (x0, y0) = List(2) {Random.nextInt(0, x); Random.nextInt(0, y)}
+        if (minedField[x0][y0] != 'X') {
+            minedField[x0][y0] = 'X'
+            mines--
+        }
     }
+    createOpenField()
 }
 
 // fun random(n: Int): List<Int> { // tried to create a randomizer function, maybe will finish in future
@@ -25,7 +54,7 @@ fun createField(x: Int, y: Int, mines: Int) {
 fun main() {
     println("How many mines do you want on the field?")
     createField(9, 9, readln().toInt())
-    for (line in minedField) {
+    for (line in OpenField) {
         println(line.joinToString(""))
     }
 }
